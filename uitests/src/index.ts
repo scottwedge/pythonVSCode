012c9d02a6025dd203d5474978ba17e8849846ea
Don Jayamanne <don.jayamanne@yahoo.com>
2019-09-23 22:55:50 +0400

    Verbose logging of ui tests

--- uitests/src/index.ts
+++ uitests/src/index.ts
@@ -28,7 +28,8 @@ const destinationOption = {
 };
 const enableVerboseLogging = {
     describe: 'Enable verbose (debug) logging',
-    default: false
+    default: false,
+    boolean: true
 };
 
 // tslint:disable-next-line: no-unused-expression
@@ -114,9 +115,9 @@ const parsedArgs = yargs
             const pythonPath =
                 argv.pythonPath === 'python'
                     ? cp
-                          .execSync('python -c "import sys;print(sys.executable)"')
-                          .toString()
-                          .trim()
+                        .execSync('python -c "import sys;print(sys.executable)"')
+                        .toString()
+                        .trim()
                     : argv.pythonPath;
             await start(argv.channel, path.resolve(argv.destination), argv.verbose, pythonPath, cucumberArgs).catch(ex => {
                 console.error('UI Tests Failed', ex);

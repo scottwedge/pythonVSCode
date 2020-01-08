bde5a0b65a97d7dc7c4e5b6c423e4b8f9993c359
Don Jayamanne <don.jayamanne@yahoo.com>
2020-01-08 13:26:01 -0800

    Oops

--- src/client/datascience/jupyter/kernels/kernelService.ts
+++ src/client/datascience/jupyter/kernels/kernelService.ts
@@ -278,13 +278,6 @@ export class KernelService {
             });
         }
         if (!kernel) {
-            // Possible user doesn't have kernelspec installed.
-            kernel = await this.getKernelSpecFromStdOut(output.stdout).catch(ex => {
-                traceError('Failed to get kernelspec from stdout', ex);
-                return undefined;
-            });
-        }
-        if (!kernel) {
             const error = `Kernel not created with the name ${name}, display_name ${interpreter.displayName}. Output is ${output.stdout}`;
             throw new Error(error);
         }
